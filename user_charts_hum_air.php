6435dda60309f64fad02f0783436c659c3ad742c
carolpoppins <carolinalopes92@gmail.com>
2016-01-19 16:30:11 +0000

    Create user_charts_hum_air.php

---
+++ user_charts_hum_air.php
@@ -0,0 +1,10 @@
+<?php
+include_once ("common/session.php");
+include_once ("common/database.php");
+include_once ("apresentacao/user_header.php");
+include_once ("apresentacao/user_lateral_menu.php");
+
+include_once ("apresentacao/user_charts_hum_air_body.php");
+//include_once ("apresentacao/user_index.php");
+
+?>
