42cb1a4cb2036a10896030f04414b94dc8ec7590
carolpoppins <carolinalopes92@gmail.com>
2016-01-19 16:21:51 +0000

    Update user_lateral_menu.php

--- apresentacao/user_lateral_menu.php
+++ apresentacao/user_lateral_menu.php
@@ -26,17 +26,29 @@
                            <nav class="nav-primary hidden-xs">
                               <div class="text-muted text-sm hidden-nav-xs padder m-t-sm m-b-sm">Start</div>
                               <ul class="nav nav-main" data-ride="collapse">
-                                 <li class="active"> <a href="user_index.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Overview</span> </a> </li>
+                                 <li class="active"> <a href="user_index.php" class="auto"> <i class="i i-stack icon"> </i> <span class="font-bold">Overview</span> </a> </li>
                                  <li >
                                     <a href="#" class="auto">
                                        <span class="pull-right text-muted">
                                           <i class="i i-circle-sm-o text"></i>
                                           <i class="i i-circle-sm text-active"></i>
                                        </span>
-                                       <i class="i i-stack icon"> </i>
+
                                        <span class="font-bold">Charts</span>
                                     </a>
                                  </li>
+                                 <li class="active"> <a href="user_charts_temp_air.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Temperature of the air</span> </a> </li>
+                                 <li >
+                                 </li>
+                                 <li class="active"> <a href="user_charts_temp_soil.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Temperature of the soil</span> </a> </li>
+                                 <li >
+                                 </li>
+                                 <li class="active"> <a href="user_charts_hum_air.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Humidity of the air</span> </a> </li>
+                                 <li >
+                                 </li>
+                                 <li class="active"> <a href="user_charts_hum_soil.php" class="auto"> <i class="i i-statistics icon"> </i> <span class="font-bold">Humidity of the soil</span> </a> </li>
+                                 <li >
+
                                  <li class="auto"> 
                                    <a href="user_pandlets.php" class="active"> <span class="pull-right text-muted"> <i class="i i-circle-sm-o text"></i> <i class="i i-circle-sm text-active"></i> </span> <i class="fa fa-map-marker"> </i> <span class="font-bold">Pandlets Location</span> </a>
 									
@@ -51,6 +63,11 @@
                               </ul>
                            </nav>
                            <!-- / nav --> 
+                        </div>
+                     </section>
+                     <footer class="footer hidden-xs no-padder text-center-nav-xs"> <a href="modal.lockme.html" data-toggle="ajaxModal" class="btn btn-icon icon-muted btn-inactive pull-right m-l-xs m-r-xs hidden-nav-xs"> <i class="i i-logout"></i> </a> <a href="#nav" data-toggle="class:nav-xs" class="btn btn-icon icon-muted btn-inactive m-l-xs m-r-xs"> <i class="i i-circleleft text"></i> <i class="i i-circleright text-active"></i> </a> </footer>
+                  </section>
+               </aside>
                         </div>
                      </section>
                      <footer class="footer hidden-xs no-padder text-center-nav-xs"> <a href="modal.lockme.html" data-toggle="ajaxModal" class="btn btn-icon icon-muted btn-inactive pull-right m-l-xs m-r-xs hidden-nav-xs"> <i class="i i-logout"></i> </a> <a href="#nav" data-toggle="class:nav-xs" class="btn btn-icon icon-muted btn-inactive m-l-xs m-r-xs"> <i class="i i-circleleft text"></i> <i class="i i-circleright text-active"></i> </a> </footer>
