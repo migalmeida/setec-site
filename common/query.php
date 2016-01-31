4f43ab0d1a245e901308b9eb8cd237c5708da38f
Miguel Almeida <migalmeida30@hotmail.com>
2016-01-31 18:04:40 +0000

    query by pandlet

--- common/query.php
+++ common/query.php
@@ -5,34 +5,34 @@
 
 class query {
 
-	function getMaxMinTempAir()
+	function getMaxMinTempAir($pandlet)
 	{
 		global $conn;
-		$query = "SELECT pandlet, date, MAX(tempair), MIN(tempair) FROM readings GROUP BY pandlet, date order by date limit 30";
+		$query = "SELECT pandlet, date, MAX(tempair), MIN(tempair) FROM readings WHERE pandlet = '$pandlet' GROUP BY pandlet, date order by date limit 30";
 		$result = pg_exec($conn, $query);
 		return $result;
 	}
 
-	function getMaxMinTempSoil()
+	function getMaxMinTempSoil($pandlet)
 	{
 		global $conn;
-		$query = "SELECT pandlet, date, MAX(tempsoil), MIN(tempsoil) FROM readings GROUP BY pandlet, date order by date limit 30";
+		$query = "SELECT pandlet, date, MAX(tempsoil), MIN(tempsoil) FROM readings WHERE pandlet = '$pandlet' GROUP BY pandlet, date order by date limit 30";
 		$result = pg_exec($conn, $query);
 		return $result;
 	}
 
-	function getMaxMinHumAir()
+	function getMaxMinHumAir($pandlet)
 	{
 		global $conn;
-		$query = "SELECT pandlet, date, MAX(humair), MIN(humair) FROM readings GROUP BY pandlet, date order by date limit 30";
+		$query = "SELECT pandlet, date, MAX(humair), MIN(humair) FROM readings WHERE pandlet = '$pandlet' GROUP BY pandlet, date order by date limit 30";
 		$result = pg_exec($conn, $query);
 		return $result;
 	}
 
-	function getMaxMinHumSoil()
+	function getMaxMinHumSoil($pandlet)
 	{
 		global $conn;
-		$query = "SELECT pandlet, date, MAX(humsoil), MIN(humsoil) FROM readings GROUP BY pandlet, date order by date limit 30";
+		$query = "SELECT pandlet, date, MAX(humsoil), MIN(humsoil) FROM readings WHERE pandlet = '$pandlet' GROUP BY pandlet, date order by date limit 30";
 		$result = pg_exec($conn, $query);
 		return $result;
 	}
